115e0ba8e3e0c4ca86f1494265199bdb0eb89304
NazimMertBilgi <nmbjobs@outlook.com.tr>
2020-06-26 15:57:52 +0300

    demo projects added.

---
+++ demo-angular/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { ImagePopup } from 'nativescript-image-popup';
+console.log(new ImagePopup().message);
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "ns-app",
+    templateUrl: "app.component.html"
+})
+export class AppComponent { }
---
+++ src/image-popup.android.ts
@@ -0,0 +1,5 @@
+import { Common } from './image-popup.common';
+
+export class ImagePopup extends Common {
+
+}
